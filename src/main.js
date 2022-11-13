530128197baccf96b37280790f7738bef8fafc1b
pmckl <peter.lajos@outlook.com>
2022-11-13 16:10:53 +0100

    add check

--- src/main.js
+++ src/main.js
@@ -1,15 +1,25 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
+
 const readfile = require('./readfile');
 const parsejson = require('./parsejson');
 const validate = require('./validate');
 
+function isPullRequest() {
+  const context = github.context;
+  console.log(context.payload)
+  return true;
+}
 async function run() {
     try {
         const schema = readfile(core.getInput('schema'));
         const config = readfile(core.getInput('config'));
         const result = validate(parsejson(schema), parsejson(config));
         if (result.length > 0) {
-            core.setFailed(result);
+          if(isPullRequest()){
+
+          }
+          core.setFailed(result);
         }
     } catch (error) {
         console.log(error);
