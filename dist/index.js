103ad17cc493a80c2480d4b286b9ede35dbc9217
pmckl <peter.lajos@outlook.com>
2022-11-13 16:38:31 +0100

    moar debug

--- dist/index.js
+++ dist/index.js
@@ -11604,7 +11604,7 @@ function getPullRequestNumber() {
 }
 function createOrUpdateComment(firstline,body){
   const octokit = github.getOctokit(githubToken)
-  console.dir(github.context.repository, { depth: null })
+  console.dir(github.context, { depth: null })
   octokit.rest.issues.listComments({
     owner: github.context.repository.owner.login,
     repo: github.context.repository.name,
